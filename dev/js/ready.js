b64281dbdd9f7d186f334eb9fe8479cd722be938
Aleksander Alekseev <shure348@gmail.com>
2021-03-02 19:27:52 +0300

    inwork

--- dev/js/ready.js
+++ dev/js/ready.js
@@ -1,4 +1,5 @@
-'use strict';
+// 'use strict';
+(function(a){'use string';a.pseudoElements={length:0};var b=function(c){if('object'==typeof c.argument||c.argument!==void 0&&c.property!==void 0){for(var d of c.elements.get()){d.pseudoElements||(d.pseudoElements={styleSheet:null,before:{index:null,properties:null},after:{index:null,properties:null},id:null});var e=function(){if(null!==d.pseudoElements.id)return+d.getAttribute('data-pe--id')!==d.pseudoElements.id&&d.setAttribute('data-pe--id',d.pseudoElements.id),'[data-pe--id="'+d.pseudoElements.id+'"]::'+c.pseudoElement;var k=a.pseudoElements.length;return a.pseudoElements.length++,d.pseudoElements.id=k,d.setAttribute('data-pe--id',k),'[data-pe--id="'+k+'"]::'+c.pseudoElement}();if(!d.pseudoElements.styleSheet)if(document.styleSheets[0])d.pseudoElements.styleSheet=document.styleSheets[0];else{var f=document.createElement('style');document.head.appendChild(f),d.pseudoElements.styleSheet=f.sheet}if(d.pseudoElements[c.pseudoElement].properties&&d.pseudoElements[c.pseudoElement].index&&d.pseudoElements.styleSheet.deleteRule(d.pseudoElements[c.pseudoElement].index),'object'==typeof c.argument){if(c.argument=a.extend({},c.argument),!d.pseudoElements[c.pseudoElement].properties&&!d.pseudoElements[c.pseudoElement].index){var g=d.pseudoElements.styleSheet.rules.length||d.pseudoElements.styleSheet.cssRules.length||d.pseudoElements.styleSheet.length;d.pseudoElements[c.pseudoElement].index=g,d.pseudoElements[c.pseudoElement].properties=c.argument}var h='';for(var i in c.argument)d.pseudoElements[c.pseudoElement].properties[i]='function'==typeof c.argument[i]?c.argument[i]():c.argument[i];for(var i in d.pseudoElements[c.pseudoElement].properties)h+=i+': '+d.pseudoElements[c.pseudoElement].properties[i]+' !important; ';d.pseudoElements.styleSheet.addRule(e,h,d.pseudoElements[c.pseudoElement].index)}else if(void 0!==c.argument&&void 0!==c.property){if(!d.pseudoElements[c.pseudoElement].properties&&!d.pseudoElements[c.pseudoElement].index){var g=d.pseudoElements.styleSheet.rules.length||d.pseudoElements.styleSheet.cssRules.length||d.pseudoElements.styleSheet.length;d.pseudoElements[c.pseudoElement].index=g,d.pseudoElements[c.pseudoElement].properties={}}d.pseudoElements[c.pseudoElement].properties[c.argument]='function'==typeof c.property?c.property():c.property;var h='';for(var i in d.pseudoElements[c.pseudoElement].properties)h+=i+': '+d.pseudoElements[c.pseudoElement].properties[i]+' !important; ';d.pseudoElements.styleSheet.addRule(e,h,d.pseudoElements[c.pseudoElement].index)}}return a(c.elements)}if(void 0!==c.argument&&void 0===c.property){var d=a(c.elements).get(0),j=window.getComputedStyle(d,'::'+c.pseudoElement).getPropertyValue(c.argument);return d.pseudoElements?a(c.elements).get(0).pseudoElements[c.pseudoElement].properties[c.argument]||j:j||null}return console.error('Invalid values!'),!1};a.fn.cssBefore=function(c,d){return b({elements:this,pseudoElement:'before',argument:c,property:d})},a.fn.cssAfter=function(c,d){return b({elements:this,pseudoElement:'after',argument:c,property:d})}})(jQuery);
 
 function number_format(number, decimals, dec_point, separator ) {
   number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
@@ -203,9 +204,25 @@ $(function(){
 
 $(function(){
   $('[data-dropdown] > button').on('click', function (event) {
+    event.preventDefault();
     $('[data-dropdown].open').removeClass('open');
     $(this).parent().toggleClass('open');
   });
+
+  $('.header_menu [data-dropdown] > a').on('click', function(event){
+    event.preventDefault();
+    $('[data-dropdown].open').removeClass('open');
+    $(this).parent().toggleClass('open');
+
+    var $this = $(this);
+    var $list = $(this).next();
+    var $wrapper = $('.header > .wrapper');
+
+    $list.css({ top: $this.offset().top - $wrapper.offset().top + 50 })
+
+    $list.cssBefore('left', `${$this.offset().left - $wrapper.offset().left - 25 + ($this.width() / 2)}px`);
+    $list.cssAfter('left', `${$this.offset().left - $wrapper.offset().left + 1 - 25 + ($this.width() / 2)}px`);
+  })
 
   $('body').on('click', function (e) {
     if (!$('[data-dropdown]').is(e.target) 
@@ -216,17 +233,3 @@ $(function(){
     }
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
